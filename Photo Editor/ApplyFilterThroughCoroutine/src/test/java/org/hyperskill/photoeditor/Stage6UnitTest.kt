fe1580cdde0b89a0aea564822963b5fc925b15de
RedJocker <mauricio.guaruja@gmail.com>
2022-05-03 14:39:02 -0300

    removing unused createBitmap() method

--- Photo Editor/ApplyFilterThroughCoroutine/src/test/java/org/hyperskill/photoeditor/Stage6UnitTest.kt
+++ Photo Editor/ApplyFilterThroughCoroutine/src/test/java/org/hyperskill/photoeditor/Stage6UnitTest.kt
@@ -377,34 +377,4 @@ class Stage6UnitTest {
 
         return  Triple(red,green,blue)
     }
-
-    fun createBitmap(): Bitmap {
-        val width = 200
-        val height = 100
-        val pixels = IntArray(width * height)
-        // get pixel array from source
-
-        var R: Int
-        var G: Int
-        var B: Int
-        var index: Int
-
-        for (y in 0 until height) {
-            for (x in 0 until width) {
-                // get current index in 2D-matrix
-                index = y * width + x
-                // get color
-                R = x % 100 + 40
-                G = y % 100 + 80
-                B = (x+y) % 100 + 120
-
-                pixels[index] = Color.rgb(R,G,B)
-
-            }
-        }
-        // output bitmap
-        val bitmapOut = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-        bitmapOut.setPixels(pixels, 0, width, 0, 0, width, height)
-        return bitmapOut
-    }
 }
